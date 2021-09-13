71bfe96f5acfacdc98d412cc7b5b4933e23931ba
Katarzyna Pikula <k.pikula@wp.eu>
2021-09-13 21:44:46 +0200

    Kalkulator2

---
+++ src/main/java/com/kodilla/kodillacourse/Kalkulator.java
@@ -0,0 +1,11 @@
+package com.kodilla.kodillacourse;
+
+public class Kalkulator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int deduct(int a, int b) {
+        return a - b;
+    }
+}
