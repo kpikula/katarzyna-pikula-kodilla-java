a8be3852a58e68be7edd96b7deb32fcf7edcf33c
Katarzyna Pikula <k.pikula@wp.eu>
2021-10-13 21:19:57 +0200

    - wersja 2

--- rps/src/main/java/rps/Logics.java
+++ rps/src/main/java/rps/Logics.java
@@ -20,11 +20,11 @@ public class Logics {
     }
 
     public void roundNumber() {
-        Scanner rounds = new Scanner(System.in);
         System.out.println("Hello, " + playerName + ", how many rounds do you want to play to win?");
+        Scanner rounds = new Scanner(System.in);
         numberOfWinningRounds = rounds.nextInt();
         System.out.println("The number of rounds to win is: " + numberOfWinningRounds);
-        System.out.println("\nInstructions: " + playerName + ", use the following keys to play the game: *1* for rock, *2* for paper, *3* for scissors, *x* to end the game, and *n* to start a new game. Let's start!\n");
+        System.out.println("\nInstructions: " + playerName + ", use the following keys to play the game: *1* for rock, *2* for paper, *3* for scissors. Let's start!\n");
     }
 
     public int playerMove() {
@@ -53,18 +53,17 @@ public class Logics {
 
     public void game() {
         while (!end) {
+            int computerWin = 0;
+            int playerWin = 0;
             for (int i = 0; i < numberOfWinningRounds; i++) {
                 System.out.println("Round: #" + (i + 1) + ". " + playerName + ", make your move!");
                 playerMove = playerMove();
                 computerMove = computerMove();
-                List<Integer> listOfResults = new ArrayList<>();
-                int computerWin = 0;
-                int playerWin = 0;
 
                 if (playerMove == computerMove) {
-                    System.out.println("\nYour move is: *" + moveName + "*, and the computer move is: *" + computerMoveName + "*. It's a tie!");
-                    playerWin = playerWin + 1;
-                    computerWin = computerWin + 1;
+                    System.out.println("Your move is: *" + moveName + "*, and the computer move is also: *" + computerMoveName + "*. It's a tie!");
+                    playerWin += 1;
+                    computerWin += 1;
                 } else if (playerMove == 1 && computerMove == 2) {
                     System.out.println("Your move is: *" + moveName + "*, and the computer move is: *" + computerMoveName + "*. Paper wraps the rock. The computer wins!");
                     playerWin = playerWin + 0;
@@ -93,37 +92,44 @@ public class Logics {
                     System.out.println("You can only use the following keys to play the game: *1* for rock, *2* for paper, *3* for scissors. Try again.\n");
                     i = i - 1;
                 }
-                System.out.println("The result is: " + playerWin + ":" + computerWin + "\n");
 
-                System.out.println("The round ended. Type *x* to end the game or type *n* to play again");
-                Scanner roundEnd = new Scanner(System.in);
-                String roundEndChoice = roundEnd.nextLine();
+                System.out.println("The result is: " + playerWin + " for you and " + computerWin + " for the computer (" + playerWin + ":" + computerWin + ")\n");
+            }
 
-                if (roundEndChoice.equals("x")) {
-                    System.out.println("Are you sure you want to end the game? [Y/N]");
-                    Scanner answerX = new Scanner(System.in);
-                    String choice = answerX.nextLine();
-                    if (choice.equals("Y"))
-                        System.out.println("The game is over");
-                    end = false;
-                    if (choice.equals("N"))
-                        System.out.println("A new round will start...");
+            System.out.println("The round ended with the final result: " + playerWin + " for you and " + computerWin + " for the computer (" + playerWin + ":" + computerWin + "). Type *x* to end the game or type *n* to play again");
+            Scanner roundEnd = new Scanner(System.in);
+            String roundEndChoice = roundEnd.nextLine();
+
+            if (roundEndChoice.equals("x")) {
+                System.out.println("Are you sure you want to end the game? [Y/N]");
+                Scanner answerX = new Scanner(System.in);
+                String choice = answerX.nextLine();
+                if (choice.equals("Y")) {
+                    System.out.println("The game is over");
                     end = true;
-
-
-                } else if (roundEndChoice.equals("n")) {
-                    System.out.println("Are you sure you want to start a new game? [Y/N]");
-                    Scanner answerN = new Scanner(System.in);
-                    String choice = answerN.nextLine();
-                    if (choice.equals("Y"))
-                        System.out.println("A new round will start...");
-                    end = true;
-                    if (choice.equals("N"))
-                        System.out.println("The game is over");
+                } else if (choice.equals("N")) {
+                    System.out.println("A new round will start...");
                     end = false;
                 }
 
+
+            } else if (roundEndChoice.equals("n")) {
+                System.out.println("Are you sure you want to start a new game? [Y/N]");
+                Scanner answerN = new Scanner(System.in);
+                String choice = answerN.nextLine();
+                if (choice.equals("Y")) {
+                    System.out.println("A new round will start...");
+                    end = false;
+                } else if (choice.equals("N")) {
+                    System.out.println("The game is over");
+                    end = true;
+                } else {
+                    System.out.println("You entered something else. Try again!");
+                    end = false;
+                }
             }
         }
     }
 }
+
+
