17604a9aa855f3b4e851bbe95056af671251cf22
kpikula <90643790+kpikula@users.noreply.github.com>
2021-11-15 12:15:50 +0100

    Flights v3

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/Flights/FlightScanner.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/Flights/FlightScanner.java
@@ -4,26 +4,26 @@ import java.util.stream.Collectors;
 
 public class FlightScanner {
     private FlightBase flightSearch = new FlightBase();
-    static private String KATOWICE = "Katowice";
+    static private String SEARCH_AIRPORT = "Wrocław";
 
     public void flightTo() {
-        System.out.println("Flights to Katowice:");
+        System.out.println("Flights to: " + SEARCH_AIRPORT);
         flightSearch.getFlight().stream()
-                .filter(flight -> flight.getArrivalAirport().contains(KATOWICE))
+                .filter(flight -> flight.getArrivalAirport().contains(SEARCH_AIRPORT))
                 .forEach(System.out::println);
     }
 
     public void flightFrom() {
-        System.out.println("\nFlights from Katowice:");
+        System.out.println("\nFlights to: " + SEARCH_AIRPORT);
         flightSearch.getFlight().stream()
-                .filter(flight -> flight.getDepartureAirport().contains(KATOWICE))
+                .filter(flight -> flight.getDepartureAirport().contains(SEARCH_AIRPORT))
                 .forEach(System.out::println);
     }
 
     public void flightThrough() {
-        System.out.println("\nFlights through Katowice:");
+        System.out.println("\nFlights to: " + SEARCH_AIRPORT);
         flightSearch.getFlight().stream()
-                .filter(flight -> flight.getArrivalAirport().contains(KATOWICE) || flight.getDepartureAirport().contains(KATOWICE))
+                .filter(flight -> flight.getArrivalAirport().contains(SEARCH_AIRPORT) || flight.getDepartureAirport().contains(SEARCH_AIRPORT))
                 .forEach(System.out::println);
     }
 }
