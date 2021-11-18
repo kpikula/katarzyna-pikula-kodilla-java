f6a85db6ba6de13df8ed7afce2b0b165fd939bd8
kpikula <90643790+kpikula@users.noreply.github.com>
2021-11-18 14:30:55 +0100

    kalkulator z wyświetlaczem v3

--- kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -1,47 +1,44 @@
 package com.kodilla.spring.calculator;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+@SpringBootTest
 public class CalculatorTestSuite {
-    //    @Autowired
+    @Autowired
+    Calculator calculator;
+
     @Test
     public void testAdd() {
         //Given
-        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
-        Calculator calculator = context.getBean(Calculator.class);
         //When
         calculator.add(45, 15);
         //Then
         //do nothing
     }
+
     @Test
     public void testSub() {
         //Given
-        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
-        Calculator calculator = context.getBean(Calculator.class);
         //When
         calculator.sub(45, 15);
         //Then
         //do nothing
     }
+
     @Test
     public void testMul() {
         //Given
-        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
-        Calculator calculator = context.getBean(Calculator.class);
         //When
         calculator.mul(45, 15);
         //Then
         //do nothing
     }
+
     @Test
     public void testDiv() {
         //Given
-        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
-        Calculator calculator = context.getBean(Calculator.class);
         //When
         calculator.div(45, 15);
         //Then
