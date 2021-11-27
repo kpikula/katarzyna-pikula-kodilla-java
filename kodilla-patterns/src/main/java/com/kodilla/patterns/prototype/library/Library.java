001f1f47e7b937862628c391a36fe65d2dd85c31
kpikula <90643790+kpikula@users.noreply.github.com>
2021-11-27 20:40:38 +0100

    Klonujemy bibliotekę v1

---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/prototype/library/Library.java
@@ -0,0 +1,66 @@
+package com.kodilla.patterns.prototype.library;
+
+import com.kodilla.patterns.prototype.Prototype;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class Library extends Prototype<Library> {
+    private String name;
+    private Set<Book> books = new HashSet<>();
+
+    public Library(final String name) {
+        this.name = name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Library)) return false;
+        Library library = (Library) o;
+        return Objects.equals(getName(), library.getName()) && Objects.equals(getBooks(), library.getBooks());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getBooks());
+    }
+
+    @Override
+    public String toString() {
+        return "Library{" +
+                "name='" + name + '\'' +
+                ", books=" + books +
+                '}';
+    }
+    public Library shallowCopy() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    public Library deepCopy() throws CloneNotSupportedException {
+        Library clonedLibrary = super.clone();
+        clonedLibrary.books = new HashSet<>();
+        for (Book theBooks : books) {
+            Book clonedList = new Book(theBooks.getTitle(), theBooks.getAuthor(), theBooks.getPublicationDate());
+            clonedLibrary.getBooks().add(clonedList);
+        }
+        return clonedLibrary;
+    }
+}
