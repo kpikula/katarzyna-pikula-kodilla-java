1eed6c79833e682a3370a7500296dbaaf2428946
kpikula <90643790+kpikula@users.noreply.github.com>
2021-11-29 19:54:38 +0100

    Analiza sposobu obliczania wartości pozycji faktury v1

--- kodilla-stream/src/main/java/com/kodilla/stream/invoice/simple/SimpleItem.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/invoice/simple/SimpleItem.java
@@ -16,6 +16,7 @@ public class SimpleItem {
     public double getQuantity() {
         return quantity;
     }
+
     public double getValue() {
         return product.getProductPrice() * quantity;
     }
--- kodilla-stream/src/test/java/com/kodilla/stream/invoice/simple/SimpleInvoiceTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/invoice/simple/SimpleInvoiceTestSuite.java
@@ -16,7 +16,25 @@ public class SimpleInvoiceTestSuite {
         invoice.addItem(new SimpleItem(new SimpleProduct("Product 2", 11.99), 3.5));
         invoice.addItem(new SimpleItem(new SimpleProduct("Product 3", 6.49), 5.0));
 
+
         //Then
         assertEquals(108.975, invoice.getValueToPay(), 0.001);
+    }
+
+    @Test
+    void getValueTest() {
+        //Given
+        SimpleItem item1 = new SimpleItem(new SimpleProduct("Product 1", 17.28), 2.0);
+        SimpleItem item2 = new SimpleItem(new SimpleProduct("Product 2", 11.99), 3.5);
+        SimpleItem item3 = new SimpleItem(new SimpleProduct("Product 3", 6.49), 5.0);
+
+        //When
+        double value1 = item1.getValue();
+        double value2 = item2.getValue();
+        double value3 = item3.getValue();
+
+        //Then
+        assertEquals(41.965, value2);
+        System.out.println(value2);
     }
 }
